108763b20ddfff815b23911cb1e7a96b33b58d03
NeilAjay <ajayghoderao@gmail.com>
2021-06-05 13:26:24 +0530

    PROJECT29

--- Block.js
+++ Block.js
@@ -4,14 +4,28 @@ class Block{
           restitution :0.4,
           friction :0.0,
       }
+      this.image = loadImage("block.png")
+      this.Visibility = 255;
       this.body = Bodies.rectangle(x, y, width, height, options);
       this.width = width;
       this.height = height;
       World.add(world, this.body);
     }
     display(){
-      var pos= this.body.position;
-      rectMode(CENTER);
-      rect(pos.x,pos.y,this.width, this.height);
+
+        //console.log(this.body.speed)
+        if(this.body.speed<3){
+          imageMode(CENTER)
+          image(this.image,this.body.position.x, this.body.position.y, this.width, this.height)
+        }
+        else{
+          World.remove(world,this.body)
+          push()
+          this.Visibility = this.Visibility-5;
+          tint(255,this.Visibility)
+          imageMode(CENTER);
+          image(this.image, this.body.position.x, this.body.position.y, 30, 40)
+          pop()
+        }
     }
 }
